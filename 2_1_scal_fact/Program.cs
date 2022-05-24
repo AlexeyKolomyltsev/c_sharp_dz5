26a8320610ece073651f8cd3c7b9dff7b14dd064
AlexeyWatt <allexdoma2@gmail.com>
2022-05-24 18:36:22 +0300

    изменил задачу 2

---
+++ 2_1_scal_fact/Program.cs
@@ -0,0 +1,38 @@
+﻿// Написать программу масштабирования фигуры
+void PrintArr(int[] array)  ///метод печати в консоль
+{
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+    {
+        if (i % 2 == 0) Console.Write($"({array[i]},");
+        else Console.Write($"{array[i]})");
+    }
+    Console.WriteLine();
+}
+
+int[] Scale(int[] array, int k)  ///метод масштабирования
+{
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+    {
+        array[i] *= k;
+    }
+    return array;
+}
+
+Console.WriteLine("Введите количество вершин фигуры"); //Задаем количество точек
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[] koordinates = new int[n * 2];
+for (int i = 0; i < n * 2; i++)   ///Заполняем массив
+{
+    Console.Write($"Введите координату для {i / 2 + 1} вершины = ");
+    koordinates[i] = int.Parse(Console.ReadLine() ?? "0");
+}
+
+PrintArr(koordinates);
+
+Console.Write("Введите коэффициент k ");
+int koef = int.Parse(Console.ReadLine() ?? "0");
+
+Scale(koordinates, koef);
+PrintArr(koordinates);
