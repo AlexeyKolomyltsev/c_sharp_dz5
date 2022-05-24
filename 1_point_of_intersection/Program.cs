608641943f490bd466ff8cd5227457bec7b61881
AlexeyWatt <allexdoma2@gmail.com>
2022-05-24 12:37:46 +0300

    добавил задачу 1

---
+++ 1_point_of_intersection/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+Console.WriteLine("Найти точку пересечения двух прямых y = k1 * x + b1, y = k2 * x + b2");
+Console.WriteLine("Введите коэффициенты b1, k1, b2 и k2:");
+float b1 = int.Parse(Console.ReadLine() ?? "0");
+float k1 = int.Parse(Console.ReadLine() ?? "0");
+float b2 = int.Parse(Console.ReadLine() ?? "0");
+float k2 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write($"Получившиеся уравнения : y = {k1}x + {b1} и y = {k2}x + {b2} ");
+if (b1 == b2 && k1 == k2) Console.WriteLine("совпадают.");
+else if (k1 == k2) Console.WriteLine("параллельны.");
+else
+{
+    float x, y;
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+    Console.WriteLine($"имеют точку пересечения с координатами ({x}, {y}).");
+}
