8b3aa7b1e53eaeb03dd8f17f1a2b4fb69798f4ec
AlexeyWatt <allexdoma2@gmail.com>
2022-05-24 18:42:43 +0300

    подправил задачу 1

--- 1_point_of_intersection/Program.cs
+++ 1_point_of_intersection/Program.cs
@@ -5,7 +5,7 @@ float b1 = float.Parse(Console.ReadLine() ?? "0");
 float k1 = float.Parse(Console.ReadLine() ?? "0");
 float b2 = float.Parse(Console.ReadLine() ?? "0");
 float k2 = float.Parse(Console.ReadLine() ?? "0");
-Console.Write($"Получившиеся уравнения : y = {k1}x + {b1} и y = {k2}x + {b2} ");
+Console.Write($"Получившиеся уравнения : y = {k1}x + {b1} и y = {k2}x + {b2}. Эти прямые ");
 if (b1 == b2 && k1 == k2) Console.WriteLine("совпадают.");
 else if (k1 == k2) Console.WriteLine("параллельны.");
 else
