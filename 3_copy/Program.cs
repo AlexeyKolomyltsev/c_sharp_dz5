2eeddc6197124e65118a416ae08c4f8422fce02c
AlexeyWatt <allexdoma2@gmail.com>
2022-05-24 13:57:38 +0300

    добавил задачу 3

---
+++ 3_copy/Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать программу копирования массива
+int[] CopyArr(int[] array)
+{   
+    int len = array.Length;
+    int[] arr_new = new int[len];
+    for(int i=0; i < len; i++)
+    {
+        arr_new[i] = array[i];
+    }
+    return array;
+}
+
+
+Console.WriteLine("Введите количество элеметов массива");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int[n];
+for(int i = 0; i < n; i++)
+{   
+    Console.WriteLine($"Введите {i} элемет массива");
+    array[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine(String.Join( " ", array));
+int[] a = CopyArr(array);
+Console.WriteLine(String.Join(" ", a));
